0c32ab03ce395968a62675a53b6310a9b8fc6c7f
colithey <colithey@gmail.com>
2024-05-09 20:40:15 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './Fonts.css';
+import './App.css';
+import { Header } from './Header';
+import { Hero } from './Hero';
+
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Hero/>
+    </>
+  );
+}
---
+++ src/Hero.jsx
@@ -0,0 +1,21 @@
+export const Hero = () => {
+    return (
+        <>
+
+            <div className="container hero__container">
+                <section className="hero" id="hero">
+                    <div className="hero__txt">
+                        <p className="hero__subtitle">new trend</p>
+                        <span className="hero__title">COLLUSION</span>
+                        <p className="hero__desc">An exclusive selection of this season's trends.</p>
+                    </div>
+                    <div className="hero__buttons">
+                        <button className="hero__btn">DISCOVER</button>
+                        <button className="hero__btn">SHOP NOW</button>
+                    </div>
+                </section>
+            </div>
+
+        </>
+    );
+}
